2c954dd20cb62664a8ce6b860c281d3c87a9038f
heerooee200 <ejgana@hotmail.com>
2020-07-17 10:09:00 -0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,7 +4,7 @@ export default{
     databaseURL: "https://comboemote.firebaseio.com",
     projectId: "comboemote",
     storageBucket: "comboemote.appspot.com",
-    messagingSenderId: "756438412027",
-    appId: "1:756438412027:web:872131c83bbc18f76f9e5e",
-    measurementId: "G-8LB9GJX1Y4"
+    messagingSenderId: "************",
+    appId: "*********",
+    measurementId: "***************"
 }
